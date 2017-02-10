c55f68e72fc6997fdef8922577913e9562fe749e
Wayne Lee <waynelee@silklabs.com>
2017-02-09 16:15:19 -0800

    Add missing config file

---
+++ config/default.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const execSync = require('child_process').execSync
+
+const sshKeyPath = path.resolve(os.homedir(), '.ssh', 'id_rsa');
+
+function getGitEmail() {
+  let stdout = execSync('git config --get user.email', { encoding: 'utf8' });
+  if (!stdout) {
+    return undefined;
+  }
+  return stdout.replace(/(\r\n|\n|\r)$/, ''); // remove newline
+}
+
+function getGitSshKey() {
+  if (!fs.existsSync(sshKeyPath)) {
+    return undefined;
+  }
+  return fs.readFileSync(sshKeyPath, 'utf8');
+}
+
+module.exports = {
+  git: {
+    userEmail: getGitEmail(),
+    sshKey: getGitSshKey(),
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
